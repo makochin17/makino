288d5961740a0116dd37a989b829a366267b2951
komine <komine@meteorites.co.jp>
2024-01-30 11:09:10 +0900

    サイドメニュー制御

--- fuel/app/views/sidemenu.php
+++ fuel/app/views/sidemenu.php
@@ -27,39 +27,42 @@
             <?php endif; ?>
           </ul>
         </li>
-        <li>
-          <span class="opener">入出庫管理</span>
-          <ul>
-            <li><?php echo Html::anchor(\Uri::create('logistics/l0010'), '入出庫一覧'); ?></li>
-            <li><?php echo Html::anchor(\Uri::create('logistics/l0011'), '入庫入力'); ?></li>
-            <li><?php echo Html::anchor(\Uri::create('logistics/l0013'), '出庫指示'); ?></li>
-            <li><?php echo Html::anchor(\Uri::create('logistics/l0012'), '出庫入力'); ?></li>
-            <li><?php echo Html::anchor(\Uri::create('location/l0010'), '保管場所検索'); ?></li>
-          </ul>
-        </li>
-        <li>
-          <span class="opener">社内管理</span>
-          <ul>
-            <li><?php echo Html::anchor(\Uri::create('schedule/s0010'), '予約スケジュール'); ?></li>
-            <li><?php echo Html::anchor(\Uri::create('schedule/s0011'), '配達予約スケジュール'); ?></li>
-            <li><?php echo Html::anchor(\Uri::create('car/c0010'), '車両情報'); ?></li>
-            <li><?php echo Html::anchor(\Uri::create('customer/c0010'), 'お客様情報'); ?></li>
-          </ul>
-        </li>
-        <li>
-          <span class="opener">マスタ管理</span>
-          <ul>
-            <li><?php echo Html::anchor(\Uri::create('mainte/m0011'), 'ユーザーマスタ'); ?></li>
-            <li><?php echo Html::anchor(\Uri::create('mainte/m0020'), 'ユニットマスタ'); ?></li>
-            <li><?php echo Html::anchor(\Uri::create('mainte/m0030'), '保管場所リレーション'); ?></li>
-          </ul>
-        </li>
+        <?php if (in_array($user_authority, array(1, 2))) : ?>
+          <li>
+            <span class="opener">入出庫管理</span>
+            <ul>
+              <li><?php echo Html::anchor(\Uri::create('logistics/l0010'), '入出庫一覧'); ?></li>
+              <li><?php echo Html::anchor(\Uri::create('logistics/l0011'), '入庫入力'); ?></li>
+              <li><?php echo Html::anchor(\Uri::create('logistics/l0013'), '出庫指示'); ?></li>
+              <li><?php echo Html::anchor(\Uri::create('logistics/l0012'), '出庫入力'); ?></li>
+              <li><?php echo Html::anchor(\Uri::create('location/l0010'), '保管場所検索'); ?></li>
+            </ul>
+          </li>
+          <li>
+            <span class="opener">社内管理</span>
+            <ul>
+              <li><?php echo Html::anchor(\Uri::create('schedule/s0010'), '予約スケジュール'); ?></li>
+              <li><?php echo Html::anchor(\Uri::create('schedule/s0011'), '配達予約スケジュール'); ?></li>
+              <li><?php echo Html::anchor(\Uri::create('car/c0010'), '車両情報'); ?></li>
+              <li><?php echo Html::anchor(\Uri::create('customer/c0010'), 'お客様情報'); ?></li>
+            </ul>
+          </li>
+          <li>
+            <span class="opener">マスタ管理</span>
+            <ul>
+              <li><?php echo Html::anchor(\Uri::create('mainte/m0011'), 'ユーザーマスタ'); ?></li>
+              <li><?php echo Html::anchor(\Uri::create('mainte/m0020'), 'ユニットマスタ'); ?></li>
+              <li><?php echo Html::anchor(\Uri::create('mainte/m0030'), '保管場所リレーション'); ?></li>
+            </ul>
+          </li>
+        <?php endif; ?>
         <li>
           <span class="opener">システム管理</span>
           <ul>
-            <li><?php echo Html::anchor(\Uri::create('system/c0080'), '会社情報設定'); ?></li>
-            <li><?php echo Html::anchor(\Uri::create('system/m0070'), '通知データメンテナンス'); ?></li>
-            <li><?php echo Html::anchor(\Uri::create('system/c0050'), 'カレンダー休日設定'); ?></li>
+            <?php if (in_array($user_authority, array(1, 2))) : ?>
+              <li><?php echo Html::anchor(\Uri::create('system/c0080'), '会社情報設定'); ?></li>
+              <li><?php echo Html::anchor(\Uri::create('system/c0050'), 'カレンダー休日設定'); ?></li>
+            <?php endif; ?>
             <li><?php echo Html::anchor(\Uri::create('auth/c0030'), 'パスワード変更'); ?></li>
           </ul>
         </li>
